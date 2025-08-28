8bd323c084ced99eecca68fdb7e93872bfb692c3
devinm0 <devinmichaels1000@gmail.com>
2025-08-28 11:56:21 -0700

    Fix sounds path.

--- public/sounds.js
+++ public/sounds.js
@@ -4,22 +4,22 @@ export class SoundManager {
       this.unlocked = false;
 
       // Preload sounds here
-      this.add("ante", "/sounds/ante.wav", 1.0); 
-      this.add("call", "/sounds/call.wav", 0.9);
-      this.add("check", "/sounds/check.wav", 0.8);
-      this.add("raise", "/sounds/raise.wav", 0.6);
-      this.add("raise10", "/sounds/raise10.wav", 0.6);
-      this.add("deal", "/sounds/deal.wav", 1.0); //
-      this.add("deal2", "/sounds/deal2.wav", 0.8);
-      this.add("deal3", "/sounds/deal3.wav", 0.8);
-      this.add("discard", "/sounds/discard.wav", 0.8);
-      this.add("discard2", "/sounds/discard2.wav", 0.8);
-      this.add("fold", "/sounds/fold.wav", 0.8);
-      this.add("fold2", "/sounds/fold2.wav", 0.8);
-      this.add("hiloselect", "/sounds/hiloselect.wav", 1.0); //
-      this.add("newhand", "/sounds/newhand.wav", 0.6);
-      this.add("start", "/sounds/start.wav", 0.1);
-      this.add("end", "/sounds/end.wav", 0.1);
+      this.add("ante", "./sounds/ante.wav", 1.0); 
+      this.add("call", "./sounds/call.wav", 0.9);
+      this.add("check", "./sounds/check.wav", 0.8);
+      this.add("raise", "./sounds/raise.wav", 0.6);
+      this.add("raise10", "./sounds/raise10.wav", 0.6);
+      this.add("deal", "./sounds/deal.wav", 1.0); //
+      this.add("deal2", "./sounds/deal2.wav", 0.8);
+      this.add("deal3", "./sounds/deal3.wav", 0.8);
+      this.add("discard", "./sounds/discard.wav", 0.8);
+      this.add("discard2", "./sounds/discard2.wav", 0.8);
+      this.add("fold", "./sounds/fold.wav", 0.8);
+      this.add("fold2", "./sounds/fold2.wav", 0.8);
+      this.add("hiloselect", "./sounds/hiloselect.wav", 1.0); //
+      this.add("newhand", "./sounds/newhand.wav", 0.6);
+      this.add("start", "./sounds/start.wav", 0.1);
+      this.add("end", "./sounds/end.wav", 0.1);
 
       // Unlock audio on first user interaction
       const unlockHandler = () => {
